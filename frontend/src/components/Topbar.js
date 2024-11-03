91515583befca7a02e4f19b82641122948f6e3e9
ThagIspace <97510320008@ispace.edu.vn>
2024-11-03 19:10:12 +0700

    fix 1 chút

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -4,15 +4,30 @@ import { FaBars } from 'react-icons/fa';
 import '../assets/style/topbar.css';
 
 const TopBar = ({ onToggleSidebar }) => {
+    const handleLogout = () => {
+        console.log('User logged out');
+        window.location.href = '/home';
+    };
+
     return (
-        <div className="topbar">
+        <div className="topbar fixed-top">
             <button
                 className="menu-icon btn btn-link"
-                onClick={onToggleSidebar} // Gọi hàm khi nhấn vào nút
+                onClick={onToggleSidebar}
             >
                 <FaBars size={24} />
             </button>
+
             <span className="topbar-title">Tổng quan</span>
+
+            <div className="logout-button ml-auto">
+                <button
+                    className="btn btn-danger"
+                    onClick={handleLogout}
+                >
+                    Đăng xuất
+                </button>
+            </div>
         </div>
     );
 };
